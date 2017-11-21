e7a432efc871e69a05825a717cb571ab94dbe2ea
Seiya Nakamura <e175711@ie.u-ryukyu.ac.jp>
2017-11-21 09:39:54 +0900

    third commit

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,14 @@ public class Main {
     public static void main (String[] args) {
         try {
             String str;
-            str = null;
-            System.out.println(str.length());
+            str = "3．14";
+            double value;
+            value = Double.parseDouble(str);
         } catch (NullPointerException e){
             System.out.println("NullPointerExceptionが発生");
             System.out.println(e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("NumberFormatExceptionが発生");
         }
     }
 }
